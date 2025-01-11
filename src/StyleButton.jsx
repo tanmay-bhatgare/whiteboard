d89b25bad1fce0e930bc22884d8b60c08e98e388
Tanmay Bhatgare <tanmaybhatgare07@gmail.com>
2025-01-11 06:30:32 +0530

    feat: websocket added

--- src/StyleButton.jsx
+++ src/StyleButton.jsx
@@ -4,8 +4,8 @@ const StyleButton = ({ iconWidth, iconHeight, iconSrc, iconAlt, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="text-blue-700 border-blue-700 hover:bg-[#181822] hover:text-white focus:outline-none 
-      font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-[#181822]"
+      className="hover:bg-[#181822] hover:text-white focus:outline-none 
+      font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:hover:bg-[#181822]"
     >
       <img width={iconWidth} height={iconHeight} src={iconSrc} alt={iconAlt} />
     </button>
